c90fe0ff20eb889758299432507d527496e1c58b
reda <20306749+redac@users.noreply.github.com>
2022-11-22 16:23:21 +0100

    chore: remove react plugin

--- index.js
+++ index.js
@@ -7,11 +7,7 @@ module.exports = {
       },
     },
   },
-  extends: [
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
+  extends: ["plugin:@typescript-eslint/recommended", "prettier"],
   rules: {
     quotes: [
       "error",
